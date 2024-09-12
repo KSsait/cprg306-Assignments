caf48fec91217fa96c21d6047808714fafdcd30c
KrisSenger <kristopher.senger@edu.sait.ca>
2024-09-12 16:25:34 -0600

    Add/create student-info component

---
+++ src/app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Student Information</h1>
+        <p>Kris Senger</p>
+        <Link href="https://github.com/KrisSenger/"><a>Back to Home</a></Link>
+      </main>
+    );
+}
