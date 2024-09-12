84048cf8c2e5f35887247a8779ed69c7791b60de
KrisSenger <kristopher.senger@edu.sait.ca>
2024-09-12 17:57:17 -0600

    Cleanup main page.js and add info

--- src/app/week-2/student-info.js
+++ src/app/week-2/student-info.js
@@ -3,9 +3,13 @@ import Link from "next/link"
 export default function StudentInfo() {
     return (
       <main>
-        <h1>Student Information</h1>
-        <p>Kris Senger</p>
-        <Link href="https://github.com/KrisSenger/"><a>Back to Home</a></Link>
+        <div>
+          <h1>Student Information</h1>
+          <p>Kris Senger</p>
+          <Link href="https://github.com/KrisSenger/">GitHub Link</Link>
+          <p></p>
+          <Link href="/">Back to Home</Link>
+        </div>
       </main>
     );
 }
