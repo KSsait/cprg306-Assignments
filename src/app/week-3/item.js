8a6625cec2f4f9e418ef818f1fb0108b652e3d57
KrisSenger <kristopher.senger@edu.sait.ca>
2024-09-26 16:51:51 -0600

    Week 4 creation and assignment - Counter

--- src/app/week-3/item.js
+++ src/app/week-3/item.js
@@ -1,6 +1,6 @@
 export default function Item({name, quantity, category}) {
     return(
-        <div className="ml-4 w-1/3 ">
+        <div className="ml-4 w-1/3">
             <li className="bg-gray-700 mb-2 list-none text-center justify-center">
                 <ul>
                     <h2 className="text-green-500 w">{name}</h2>
---
+++ src/app/week-4/page.js
@@ -0,0 +1,10 @@
+import Counter from './new-item';
+
+export default function Page() {
+    return (
+        <main className='bg-black'>
+            <h1 className="text-center text-6xl m-10">Counter Component</h1>
+            <Counter />
+        </main>
+    );
+}
