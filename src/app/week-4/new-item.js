8237470afb621fa87d4d35d5c4df45ddf3312624
KrisSenger <kristopher.senger@edu.sait.ca>
2024-10-01 15:16:53 -0600

    Rework for centering the element

--- src/app/week-4/new-item.js
+++ src/app/week-4/new-item.js
@@ -8,10 +8,24 @@ export default function Counter(){
     const increment = () => { setCount(count + 1);}
     const decrement = () => { setCount(count - 1);}
     return(
-        <div className="text-center">
-                <input className="inline-flex relative w-60 h-20 mr-2 text-6xl bg-gray-500 rounded-lg" type="text" value={count} />
-                <button disabled={count==20} className="text-green-500 text-6xl bg-purple-600 size-16 hover:bg-gray-400 rounded-lg mr-2" onClick={increment}>+</button>
-                <button disabled={count==1} className="text-red-500 text-6xl bg-yellow-300 size-16 mt-6 hover:bg-gray-400 rounded-lg" onClick={decrement}>-</button>
+        <div className="relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-row justify-center items-center max-w-[250px]">
+            <input 
+                className="w-full h-20 mr-2 text-6xl bg-gray-500 rounded-lg" 
+                type="text" 
+                value={count} 
+            />
+            <button 
+                disabled={count==20} 
+                className="absolute right-20 text-green-500 text-6xl bg-purple-600 size-16 hover:bg-gray-400 rounded-lg mr-2" 
+                onClick={increment}>
+                +
+            </button>
+            <button 
+                disabled={count==1} 
+                className="absolute text-center right-4 text-red-500 text-6xl bg-yellow-300 size-16 hover:bg-gray-400 rounded-lg" 
+                onClick={decrement}>
+                -
+            </button>
         </div>
     );
 }
--- src/app/week-4/page.js
+++ src/app/week-4/page.js
@@ -3,7 +3,7 @@ import Counter from './new-item';
 export default function Page() {
     return (
         <main className='bg-black'>
-            <h1 className="text-center text-6xl m-10">Counter Component</h1>
+            <h1 className="text-center text-6xl m-10 mb-20">Counter Component</h1>
             <Counter />
         </main>
     );
