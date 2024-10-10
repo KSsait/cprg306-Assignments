55b0145a16bed363824699c524130ce3c2df8965
KrisSenger <kristopher.senger@edu.sait.ca>
2024-10-10 14:11:23 -0600

    week 6 intial fix

---
+++ src/week-6/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <main className='bg-black h-screen'>
+            <h1 className="text-center text-6xl m-10 mb-20">Week 6 - Interaction with Forms</h1>
+            
+        </main>
+    );
+}
