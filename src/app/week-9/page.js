c70be169ff0eb232976d962138e625c3b0bc6bfa
KrisSenger <kristopher.senger@edu.sait.ca>
2024-11-07 11:56:18 -0700

    Add redirection with login/sign-out

--- src/app/week-9/page.js
+++ src/app/week-9/page.js
@@ -1,34 +1,39 @@
 "use client"
 
 import { useUserAuth } from "./_utils/auth-context";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
   const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+  const router = useRouter();
 
   const signIn = async () => {
     await gitHubSignIn();
+    router.push("week-9/shopping-list");
   }
   const signOut = async () => {
     await firebaseSignOut();
+    router.push("week-9/");
   }
 
   console.log(user);
 
   return(
       <main>
-          <h1 className="font-bold text-3xl mb-5">Shopping List</h1>
-          <h2 className="text-xl mb-5">Login Page </h2>
-          {user ? ( 
-            <div>
-              <p className="m-4"> Welcome, {user.displayName} </p>
-              <a href="/week-9/shopping-list">Go to Shopping List </a>
-              <div>
-                <button onClick={signOut}>Sign Out</button>
-              </div>
+        <h1 className="font-bold text-3xl mb-5">Shopping List</h1>
+        <h2 className="text-xl mb-5">Login Page </h2>
+        {user ? ( 
+          <div>
+            <p className="m-4"> Welcome, {user.displayName} </p>
+            <Link href="/week-9/shopping-list">Go to Shopping List </Link>
+            <div className="mt-5 text-right">
+              <button onClick={signOut}>Sign Out</button>
             </div>
-          ) : (
-            <button onClick={signIn}>Sign In using Github</button>
-          )}
+          </div>
+        ) : (
+          <button onClick={signIn}>Sign In using Github</button>
+        )}
           
       </main>
   )
--- src/app/week-9/shopping-list/page.js
+++ src/app/week-9/shopping-list/page.js
@@ -6,11 +6,13 @@ import itemsData from "./items.json";
 import { useState } from "react";
 import MealIdeas from "./meal-ideas";
 import { useUserAuth } from "../_utils/auth-context";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
     const [items, setItems] = useState(itemsData);
     const [selectedItemName, setSelectedItemName] = useState("");
-    const { user } = useUserAuth();
+    const { user, firebaseSignOut } = useUserAuth();
+    const router = useRouter();
 
     const handleAddItem = (item) => {
         setItems([...items, item]);
@@ -20,6 +22,11 @@ export default function Page() {
         const itemReplace = item.name.replace(/[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]| /g,'').trim().split(",")[0];
         setSelectedItemName(itemReplace);
     }
+
+    const signOut = async () => {
+        await firebaseSignOut();
+        router.push("/week-9");
+      }
 
     if (!user) {
         return (
@@ -32,12 +39,17 @@ export default function Page() {
         );
     }
 
+
     return (
         <main className="bg-black h-screen flex">
             <div className="flex flex-col m-5">
                 <h1 className="font-bold text-2xl text-center mb-4">Shopping List</h1>
                 <NewItem onAddItem={handleAddItem}/>
                 <ItemList items={items} onItemSelect={handleItemSelect}/>
+            </div>
+
+            <div className="absolute right-0 top-0 m-3">
+                <button onClick={signOut}>Sign Out</button>
             </div>
 
             <div className="ml-5 mt-80">   
